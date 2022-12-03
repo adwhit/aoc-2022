230928af139385e30faed23d4f8099752e313e87
Alex Whitney <adwhit@fastmail.com>
2022-12-03 21:23:22 +0000

    lazy day3

---
+++ day3.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+total = 0
+with open("day3.data") as f:
+    rows = list(l.strip() for l in f)
+
+def priority(val):
+    if val.upper() == val:
+        return ord(val) - ord("A") + 27
+    else:
+        return ord(val) - ord("a") + 1
+
+for line in rows:
+    left = set(line[:len(line)//2])
+    right = set(line[len(line)//2:])
+    common = left & right
+    assert len(common) == 1
+    val = priority(common.pop())
+    total += val
+print(total)
+
+ix = 0
+total2 = 0
+while ix < len(rows):
+    common = set(rows[ix]) & set(rows[ix + 1]) &  set(rows[ix + 2])
+    ix += 3
+    assert len(common) == 1
+    total2 += priority(common.pop())
+print(total2)
